f74f0bf33ddff038d1251c52351318a4ab6fc39d
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-05-10 08:40:30 -0400

    js pseudocode started

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//globalVariables
+//submit button
+//input field
+
+
+//Function for formHandling - called by event listener for the submit button
+//function for initial API call (trailSearch) - fetch initial API  
+//pass data to new function to display the info on the page 
+
+
+//function for second API call (weatherSearch) to get weather by zip code - get respose as an object or array
+//function to display page content - change classes of search result containers to visible, append content to page 
