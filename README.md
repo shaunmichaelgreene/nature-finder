abf5abdad2312cbe309ea42b1c93138df993e834
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-05-18 00:27:27 -0400

    js refactor complete, readme updated, new modal added to HTML

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Nature Finder is an application that will allow you to search a zipcode to find 
 
 This app utilizes HTML, CSS, JavaScript and multiple APIs to create a single-page application to find nearby parks or trails. The user enters their zip code and is shown the name, address, phone #, and website (if applicable) for up to 5 natural POIs in a 5km radius. The zip code is passed to a function that fetches a positionstack API to return the geographical coordinates and city/state name. A second function then uses the coordinates to fetch the POI data from a Bing Maps API. A 3rd API with Bing Maps was installed as a backup to support the positionstack API, as the free version of positionstack does not support HTTPS encryption, which is required by GitHub Pages. As a result, the positionstack API is only usable in a test environment. 
 
+**Presentation:** https://docs.google.com/presentation/d/1fCUpbm_U-_Q4L0K-1IIRDDiC1S_TNNcZ-WKClF4j65M/edit?usp=sharing
+
 ## Live Deployed Link:
 https://shaunmichaelgreene.github.io/easy-trail-finder/
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -10,57 +10,45 @@ var displayCount = 0;
 var clearHistoryButtonEl = document.querySelector("#clear-history");
 
 
-var formSubmitHandler = function(event) {
+var formSubmitHandler = function(event) { //called by event listener on search form submit button
     event.preventDefault();
-    console.log("the submit button was clicked!")
-    // resultsContainer.textContent="";
     var zipInput = zipInputEl.value.trim( );
-    console.log(zipInput);  
     if (isNaN(zipInput)) { //ERROR HANDLING TO DETECT NON-NUMERIC CHARACTERS IN THE INPUT
         $('#input-alert').foundation('open');
         zipInputEl.value = ""         
-
-        // alert("Please re-enter your search term as a 5-digit zip code (Ex:'15212'");
-        // location.reload();
     } else if (zipInput.length == 5) { //check to verify if input is a 5-digit zipcode
         console.log("A search has been initialized for the zip code of: " + zipInput);
         zipInputEl.value = ""; 
         getCoordinates(zipInput); //pass zip to function to retrieve coordinates
-        // getTrailInfo(zipInput);
-    } else { //if input invalid, (ex: trigger user MODAL to re-enter)
+    } else { //if input invalid, trigger input-alert modal 
         $('#input-alert').foundation('open'); 
         zipInputEl.value = ""         
     }  
 };
 
-var updateSearchHistory = function(cityName, stateId, zipInput) {
-    var newSearch = {
+var updateSearchHistory = function(cityName, stateId, zipInput) { 
+    var newSearch = { //create new object to save search parameters
         city: cityName,
         state: stateId,
         zip: zipInput
     }
-    console.log(newSearch);
-    searchHistory.push(newSearch);
+    // console.log(newSearch);
+    searchHistory.push(newSearch); 
     localStorage.setItem("searchHistory", JSON.stringify(searchHistory)); //set search history in localStorage
-    var historyButtonEl = document.createElement("button");
+    var historyButtonEl = document.createElement("button"); //create new button for the saved search
     $(historyButtonEl).addClass("button"); 
     historyButtonEl.textContent = zipInput.concat(" (", cityName, ", ", stateId, ")");
     historyButtonEl.setAttribute("id", zipInput);
     console.log(zipInput.concat(" (", cityName, ", ", stateId, ")"));
     buttonsContainerEl.appendChild(historyButtonEl);
-    clearHistoryButtonEl.classList.remove("not-shown");
+    clearHistoryButtonEl.classList.remove("not-shown"); //reveal clear-history button
 }
 
-
-// var displaySearchButtons = function(newSearch) {
-// console.log(newSearch);
-// };
-
-var loadSearchHistory = function(zipInput) {
-    searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+var loadSearchHistory = function() { //called upon page load
+    searchHistory = JSON.parse(localStorage.getItem("searchHistory")); //retrieve search history array from localStorage
     if (!searchHistory){
         searchHistory = [];
-    } else {
+    } else { //create new variables for the attribute data of each object in the array, and use that data to create and append a new button to the page for that search
         $.each(searchHistory, function (e) {
             var cityName = $(this).attr("city");
             var stateId = $(this).attr("state");
@@ -70,33 +58,31 @@ var loadSearchHistory = function(zipInput) {
             historyButtonEl.textContent = (zip + " (" + cityName + ", " + stateId + ")");
             historyButtonEl.setAttribute("id", zip);
             buttonsContainerEl.appendChild(historyButtonEl);
-            clearHistoryButtonEl.classList.remove("not-shown");
+            clearHistoryButtonEl.classList.remove("not-shown"); //if a search history exists, show the clear-history button
         })
     };
 }
 
-var getCoordinates = function(zipInput) {
-    console.log(zipInput);
-    var apiKey = "3e833f7a376ea50a4b5e624e59d6e907"
-    var apiUrl = "http://api.positionstack.com/v1/forward?access_key=" + apiKey + "&query=" + zipInput + "," + "US";
+var getCoordinates = function(zipInput) { //called by form submit handler
+    var apiKey = "3e833f7a376ea50a4b5e624e59d6e907" 
+    var apiUrl = "http://api.positionstack.com/v1/forward?access_key=" + apiKey + "&query=" + zipInput + "," + "US"; //positionstack api uses zip code to retrieve coordinates
     fetch(apiUrl)
         .then(function(response) {
             if (response.ok) { 
                 response.json().then(function(data) { 
-                    console.log(data);
-                    var latitude = data.data[0].latitude;
+                    // console.log(data);
+                    var latitude = data.data[0].latitude; //create new local variables for response data
                     var longitude = data.data[0].longitude;
                     var cityName = data.data[0].locality;
                     var stateId = data.data[0].region_code;
                     console.log(latitude, longitude, cityName, stateId);
-                    console.log(cityName + ', ' + stateId);
-                    getTrailInfo(latitude, longitude);
+                    getTrailInfo(latitude, longitude); 
                     updateSearchHistory(cityName, stateId, zipInput); //pass zip to search history
                     nameContainerEl.textContent = ("Showing Results for: " + zipInput + " (" + cityName + ", " + stateId + ")");
             });
             } else {
                 console.log("Error: " + response.statusText, " passing parameters to backup API function");
-                getCoordinatesPlanB(zipInput);
+                getCoordinatesPlanB(zipInput);  //if fetch fails due to insecure request, call backup API function
             };
         })    
         .catch(function(error) {
@@ -109,17 +95,17 @@ var getCoordinates = function(zipInput) {
 var getCoordinatesPlanB = function(zipInput) {
     var apiKey = "AtVPCFspco6yXG6X2mdr0-y2H4XO7ZPHdP_Tj-jLPHoBCXKiJlNCsG41H7GSWoYl";
     var apiUrl = "http://dev.virtualearth.net/REST/v1/Locations?q=" + zipInput + "&key=" + apiKey;
-    fetch(apiUrl)
+    fetch(apiUrl) //bing maps location API
         .then(function(response) {
             if (response.ok) { 
                 response.json().then(function(data) { 
-                    console.log(data);
+                    // console.log(data); 
+                    //create new local variables for response data
                     var latitude = data.resourceSets[0].resources[0].point.coordinates[0];
                     var longitude = data.resourceSets[0].resources[0].point.coordinates[1];
                     var cityName = data.resourceSets[0].resources[0].address.locality;
                     var stateId = data.resourceSets[0].resources[0].address.adminDistrict;
                     console.log(latitude, longitude, cityName, stateId);
-                    console.log(cityName + ', ' + stateId);
                     getTrailInfo(latitude, longitude);
                     updateSearchHistory(cityName, stateId, zipInput); //pass zip to search history
                     nameContainerEl.textContent = ("Showing Results for: " + zipInput + " (" + cityName + ", " + stateId + ")");
@@ -129,7 +115,7 @@ var getCoordinatesPlanB = function(zipInput) {
             };
         })    
         .catch(function(error) {
-            $('#api-alert').foundation('open');
+            $('#api-alert').foundation('open');  //if fetch fails, trigger api-alert modal
         });    
 
 }
@@ -137,7 +123,6 @@ var getCoordinatesPlanB = function(zipInput) {
 //ANCHOR LINK TO SEARCH FOR NAME AND ZIP CODE ON GOOGLE MAPS? 
 
 var getTrailInfo = function(latitude, longitude) {
-    // var secretKey = 
     var apiKey = "AtVPCFspco6yXG6X2mdr0-y2H4XO7ZPHdP_Tj-jLPHoBCXKiJlNCsG41H7GSWoYl";
     var apiUrl = "https://dev.virtualearth.net/REST/v1/LocalSearch/?type=Parks&userCircularMapView=" + latitude + "," + longitude + "," + "5000&key=" + apiKey; //returns POIs like community centers, landmarks, farms, etc. NO TRAILS OR PARKS.
     // var apiUrl = "https://dev.virtualearth.net/REST/v1/LocalSearch/?query=park&userCircularMapView=" + latitude + "," + longitude + "," + "5000&key=" + apiKey; search one parameter at a time - PARK | TRAIL
@@ -147,13 +132,10 @@ var getTrailInfo = function(latitude, longitude) {
             if (response.ok) { 
                 response.json().then(function(data) { 
                     console.log(data);
-                    if (data.resourceSets[0].resources.length == 0) {
+                    if (data.resourceSets[0].resources.length == 0) { //if no results in the response, trigger a modal alert
                         console.log("No results found in your area! Go blaze your own trail! (or try a different location)");
-                        //call new function to clear page content?
-                        //add modal
-                    } else if (data.resourceSets[0].resources.length > 0) {
-                        console.log(data.resourceSets[0].resources[0].Address.formattedAddress);
-                        console.log(data.resourceSets[0].resources.length);
+                        $('#no-results').foundation('open');
+                    } else if (data.resourceSets[0].resources.length > 0) { //if results found, pass response data to new function
                         buildTrailArrays(data);                        
                     }
                 });
@@ -169,27 +151,21 @@ var getTrailInfo = function(latitude, longitude) {
 };
     
 var buildTrailArrays = function(data) {
-    var resultsObject = {
+    var resultsObject = { //create new local object variable of empty arrays, for each response property
         nameArr: [],
         addressArr: [],
         phoneArr: [],
         websiteArr:[]
     };
     
-    for (var i=0; i < data.resourceSets[0].resources.length; i++) {
+    for (var i=0; i < data.resourceSets[0].resources.length; i++) { //use loop to populate the arrays with each response value
         // console.log(data.resourceSets[0].resources[i]);
         resultsObject.nameArr.push(data.resourceSets[0].resources[i].name);
         resultsObject.addressArr.push(data.resourceSets[0].resources[i].Address.formattedAddress);
         resultsObject.phoneArr.push(data.resourceSets[0].resources[i].PhoneNumber);
         resultsObject.websiteArr.push(data.resourceSets[0].resources[i].Website);
         
-        // name: data.resourceSets[0].resources[i].name,
-        // address: data.resourceSets[0].resources[i].Address.formattedAddress,
-        // phone: data.resourceSets[0].resources[i].PhoneNumber,
-        // website: data.resourceSets[0].resources[i].Website
-        // var resultWebsite =  resultsObject.Website;
-        // var resultPhone = resultsObject.PhoneNumber;
-        
+        // potential error handling to determine if response includes geocode data or not
         // if (data.resourceSets[0].resources[i].point.coordinates) {
         //     var resultCoordinates = resultsObject.point.coordinates;
         //     console.log(resultCoordinates);
@@ -197,39 +173,36 @@ var buildTrailArrays = function(data) {
         //     console.log("no coordinates provided");
         // };
     };
-    console.log(resultsObject);
-    displayTrailInfo(resultsObject)
+    // console.log(resultsObject);
+    displayTrailInfo(resultsObject) // pass new object of data arrays to display function
 };
 
 var displayTrailInfo = function(resultsObject) {
-    console.log(resultsObject); //replace with data/or data response object
-    //add dark border class to all divs (to make them appear visible)
-    // displayCount++
-    // if (displayCount <= 5) {
-        var nameIdPrefix = "name";
-        var addressIdPrefix = "address";
-        var phoneIdPrefix = "phone";
-        var websiteIdPrefix = "website";
-        var resultRowIdPrefix = "results-row"
-    for (var i = 0; i < 5; i++) {
+    //create local variables to hold element ID prefixes
+    var nameIdPrefix = "name";
+    var addressIdPrefix = "address";
+    var phoneIdPrefix = "phone";
+    var websiteIdPrefix = "website";
+    var resultRowIdPrefix = "results-row";
+    for (var i = 0; i < 5; i++) { //loop 5x to create new variables with target element IDs to insert response data into page
         var targetNameId = nameIdPrefix.concat("-", (i+1));
         var targetAddressId = addressIdPrefix.concat("-", (i+1));
         var targetPhoneId = phoneIdPrefix.concat("-", (i+1));
         var targetWebsiteId = websiteIdPrefix.concat("-", (i+1));
         var targetResultRowId = resultRowIdPrefix.concat("-", (i+1));
                
-        if (!resultsObject.nameArr[i]) {
+        if (!resultsObject.nameArr[i]) { //if the name array is empty for a new index, then there will be no other results for that matching index in the other arrays, so the entire row can be hidden
             document.getElementById(targetNameId).textContent = "No Result Found!";
             document.getElementById(targetAddressId).textContent = "No Result Found!";
             document.getElementById(targetPhoneId).textContent = "No Result Found!";
             document.getElementById(targetWebsiteId).textContent = "No Result Found!";
             document.getElementById(targetResultRowId).classList.add("not-shown");
             
-        } else {
+        } else {  //if the array does have content, display it on the page
             document.getElementById(targetNameId).textContent = resultsObject.nameArr[i];
 
         }
-        if (!resultsObject.addressArr[i]) {
+        if (!resultsObject.addressArr[i]) { //run this same check for each property
             document.getElementById(targetAddressId).textContent = "No Address Found!";
 
         } else {
@@ -243,50 +216,36 @@ var displayTrailInfo = function(resultsObject) {
             document.getElementById(targetPhoneId).textContent = resultsObject.phoneArr[i];
         }
         
-        if (!resultsObject.websiteArr[i]) {
+        if (!resultsObject.websiteArr[i]) { 
             document.getElementById(targetWebsiteId).textContent = "No Website Found!";
         } else {
             document.getElementById(targetWebsiteId).textContent = "website";
             document.getElementById(targetWebsiteId).setAttribute("href", resultsObject.websiteArr[i]);
             document.getElementById(targetWebsiteId).setAttribute("target", "_blank");
         }
-        //LOOK INTO ADDING CLASS/ID OF "RESULS-ROW-0, RESULTS-ROW-1, ETC" 
 
-        // resultsContainerEl.classList.remove("not-shown");
-        console.log(resultsObject.websiteArr[i]);
-
-
+        // console.log(resultsObject.websiteArr[i]);
     };
-    resultsContainerEl.classList.remove("not-shown");
-    clearHistoryButtonEl.classList.remove("not-shown")
+    resultsContainerEl.classList.remove("not-shown"); //remove hidden class to display content on page
+    clearHistoryButtonEl.classList.remove("not-shown") //remove hidden class to display clear-history button on page
 
 };
 
-var buttonClickHandler = function(event) {
-    var zipInput = event.target.getAttribute("id")
-    if(zipInput.length == 5) {
-        resultsContainerEl.classList.remove("not-shown");
-        // $("#results-container").empty();
-        getCoordinates(zipInput);
-;    } else if (zipInput = "clear-history") {
-        localStorage.clear()
-        location.reload()
+var buttonClickHandler = function(event) { //when search history button is clicked
+    var zipInput = event.target.getAttribute("id") //locate the button's ID and make it the zipInput value
+    if(zipInput.length == 5) { //verify button clicked was for a search history button with a zip code as the ID
+        resultsContainerEl.classList.remove("not-shown"); //remove hidden class from search results container
+        getCoordinates(zipInput); //pass zip code to function to pull coordinates and repeat process
+;    } else if (zipInput = "clear-history") { //if the clear-history button is clicked instead, clear local storage and refresh page
+        localStorage.clear() 
+        location.reload() 
     };
 };
 
 loadSearchHistory()
 
 searchFormEl.addEventListener("submit", formSubmitHandler); //event listener for initial search button
-buttonsContainerEl.addEventListener("click", buttonClickHandler);
+buttonsContainerEl.addEventListener("click", buttonClickHandler);  //event listener for search history buttons
 
 
-//API KEY SECRET - ADD PLACEHOLDER FOR YOUR API KEY. PUSH TO GH SO THAT IT ISNT VIEWABLE. THEN MAKE NOTE IN THE README POINT TO WHERE YOUR API KEY. 
-
-//Function for formHandling - called by event listener for the submit button
-//function for initial API call (trailSearch) - fetch initial API  
-//pass data to new function to display the info on the page 
-
-
-//function for second API call (weatherSearch) to get weather by zip code - get respose as an object or array
-//function to display page content - change classes of search result containers to visible, append content to page 
-
+//API KEY SECRET - ADD PLACEHOLDER FOR YOUR API KEY ONCE PRESENTATION IS OVER. PUSH TO GH SO THAT IT ISNT VIEWABLE. THEN MAKE NOTE IN THE README POINT TO WHERE YOUR API KEY. 
